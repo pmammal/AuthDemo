0d8f24e3255f243fe190e3630cc6ce493aea7f33
Elliott Wilder <elliottjwilder@gmail.com>
2021-02-04 15:13:31 -0600

    add bcrypt hash

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const app = express();
-const USer = require('./models/user');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
+
+mongoose
+	.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('mongo connection open!!');
+	})
+	.catch((err) => {
+		console.log('oh no mongo ERROR!');
+		console.log(err);
+	});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.send('homepage');
+});
 app.get('/register', (req, res) => {
 	res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+	const { password, username } = req.body;
+	const hash = await bcrypt.hash(password, 12);
+	const user = new User({
+		username,
+		password : hash
+	});
+	await user.save();
+	res.redirect('/');
 });
 
 app.get('/secret', (req, res) => {
