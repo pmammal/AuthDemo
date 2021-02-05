77254780a6f646e204f7dab6ee6fec581021c70d
Elliott Wilder <elliottjwilder@gmail.com>
2021-02-05 11:03:35 -0600

    fix punc error

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ userSchema.statics.findAndValidate = async function (username, password) {
 };
 
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next():
+	if (!this.isModified('password')) return next();
 	this.password = await bcrypt.hash(this.password, 12);
 	next();
 });
